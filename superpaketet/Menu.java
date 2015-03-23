af5622e6917c86b0de045191e70ce43389407386
Anton <wahlberg.anton@gmail.com>
2015-03-23 15:24:02 +0100

    Hide result pane

--- superpaketet/Menu.java
+++ superpaketet/Menu.java
@@ -9,18 +9,20 @@ import javax.xml.ws.Holder;
 
 public class Menu extends JFrame implements ActionListener{
 	
-	protected static JButton browse, edit, b1, b2, b3, b4, b5, b6, b7, b8, b9, b10, b11, b12; 
-	GridLayout mainMenuGrid = new GridLayout(1,2);
-	GridLayout subMenuGrid = new GridLayout(3,2);
+	GridLayout mainMenuGrid 			= new GridLayout(1, 2, 4, 4);
+	GridLayout subMenuGrid 				= new GridLayout(3, 2, 4, 4);
 	
-	static MainPanel mainMenu = new MainPanel();
-	static BrowsingPanel browsingMenu = new BrowsingPanel();
-	static EditingPanel editingMenu = new EditingPanel();
-	static JPanel resultPane = new JPanel(new BorderLayout());
+	static MainPanel mainMenu 			= new MainPanel();
+	static BrowsingPanel browsingMenu 	= new BrowsingPanel();
+	static EditingPanel editingMenu 	= new EditingPanel();
+	static DatabaseHandler dbh 			= new DatabaseHandler();
+	static Menu window 					= new Menu("Menysystem");
 	
-	static JFrame frame = new JFrame("Banandannyprogrammet!");
-	static DatabaseHandler dbh = new DatabaseHandler();
-	static Menu menu = new Menu("Menysystem");
+	static JPanel backgroundLeft 		= new JPanel(new BorderLayout());
+	static JPanel backgroundRight 		= new JPanel(new BorderLayout());
+	static JPanel resultPane 			= new JPanel(new BorderLayout()); 
+	static JScrollBar scrollBar				= new JScrollBar(JScrollBar.VERTICAL, 30, 40, 0, 300);
+	
 	
 	public Menu(String title){
 		
@@ -35,21 +37,25 @@ public class Menu extends JFrame implements ActionListener{
 	private static void createAndShowGUI(){
 
 		
-		Container contentPane = menu.getContentPane();
-		contentPane.add(mainMenu, BorderLayout.PAGE_START);
-		contentPane.add(browsingMenu, BorderLayout.LINE_START);
-		contentPane.add(editingMenu, BorderLayout.LINE_END);
-		contentPane.add(resultPane, BorderLayout.CENTER);
+		Container contentPane = window.getContentPane();
+		contentPane.add(backgroundLeft, BorderLayout.LINE_START);
+		contentPane.add(backgroundRight, BorderLayout.LINE_END);
+		backgroundLeft.add(mainMenu, BorderLayout.PAGE_START);
+		backgroundLeft.add(browsingMenu, BorderLayout.LINE_START);
+		backgroundLeft.add(editingMenu, BorderLayout.LINE_END);
+		backgroundRight.add(resultPane, BorderLayout.CENTER);
 		browsingMenu.setVisible(false);
 		editingMenu.setVisible(false);
-		menu.setVisible(true);
+		backgroundLeft.setVisible(true);
+		backgroundRight.setVisible(true);
+		window.setVisible(true);
 		browsingMenu.b3.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent event) {
 				listOfNames();
 				resultPane.setVisible(true);
 				//mainMenuPane.setVisible(false);
 				browsingMenu.setVisible(false);
-				//frame.pack();
+				window.pack();
 				System.out.println("Edit");
 			} });
 		
@@ -57,15 +63,16 @@ public class Menu extends JFrame implements ActionListener{
 			public void actionPerformed(ActionEvent event) {
 				browsingMenu.setVisible(false);
 				editingMenu.setVisible(true);
-				menu.pack();
+				window.pack();
 				System.out.println("Edit");
 			} });
 		
 		mainMenu.browse.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent event) {
+				resultPane.setVisible(false);
 				editingMenu.setVisible(false);
 				browsingMenu.setVisible(true);
-				menu.pack();
+				window.pack();
 				System.out.println("Browse");
 			} });
 		
@@ -79,66 +86,6 @@ public class Menu extends JFrame implements ActionListener{
 		menu.addContentEditPane(); */
 	}
 	
-/*	public void addContentBrowsePane(){
-		
-		b1 = new JButton("Search");
-		//b1.setVerticalTextPosition(AbstractButton.CENTER);
-		//b1.setHorizontalTextPosition(AbstractButton.LEADING);
-		b1.setToolTipText("This is the first alternative of this menu.");
-		//b1.addActionListener(this);
-		
-		b2 = new JButton("Search by ID");
-		b2.setToolTipText("BEHOLD! This is the second alternative of this menu.");
-		
-		b3 = new JButton("Search by family name");
-		b3.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent event) {
-				listOfNames();
-				resultPane.setVisible(true);
-				//mainMenuPane.setVisible(false);
-				browseMenuPane.setVisible(false);
-				//frame.pack();
-				System.out.println("Edit");
-			} });
-			
-		b4 = new JButton("List by ID");
-		
-		b5 = new JButton("List by family name");
-		
-		b6 = new JButton("Info about a team");
-		
-		browseMenuPane.add(b1);
-		browseMenuPane.add(b2);
-		browseMenuPane.add(b3);
-		browseMenuPane.add(b4);
-		browseMenuPane.add(b5);
-		browseMenuPane.add(b6);
-		//frame.setContentPane(browseMenuPane);
-		//frame.getContentPane().remove(mainMenuPane);
-		//frame.getContentPane().add(browseMenuPane, BorderLayout.CENTER);
-		//browseMenuPane.setVisible(true);
-	}
-	
-	public void addContentEditPane(){
-	
-		b7 = new JButton("Create new member");
-		b8 = new JButton("Update existing member");
-		b9 = new JButton("Activate/deactivate member");
-		b10 = new JButton("Update mail");
-		b11 = new JButton("Update function");
-		b12 = new JButton("Delete member");
-		editMenuPane.add(b7);
-		editMenuPane.add(b8);
-		editMenuPane.add(b9);
-		editMenuPane.add(b10);
-		editMenuPane.add(b11);
-		editMenuPane.add(b12);
-		//editMenuPane.setVisible(true);
-		//frame.setContentPane(editMenuPane);
-		//frame.getContentPane().add(editMenuPane, BorderLayout.CENTER);
-		
-	}
-	*/
 	public void actionPerformed(ActionEvent e) {}
 	
 	
@@ -147,6 +94,7 @@ public class Menu extends JFrame implements ActionListener{
 		JList<String> list = new JList(listData.toArray());
 		resultPane.removeAll();
 		resultPane.updateUI();
+		resultPane.add(scrollBar, BorderLayout. LINE_END);
 		resultPane.add(list, BorderLayout.CENTER);
 	}
 	
@@ -214,7 +162,7 @@ class BrowsingPanel extends JPanel implements ActionListener{
 				Menu.listOfNames();
 				Menu.resultPane.setVisible(true);
 				//mainMenuPane.setVisible(false);
-				Menu.browsingMenu.setVisible(false);
+				setVisible(false);
 				//frame.pack();
 				System.out.println("Edit");
 			} }); */
@@ -268,3 +216,76 @@ class EditingPanel extends JPanel implements ActionListener{
 	public void actionPerformed(ActionEvent e) {}
 
 }
+/* 
+class resultPanel extends JPanel implements ActionListener{
+
+	public resultPanel(){
+		
+		
+		
+	}
+	
+	public void actionPerformed(ActionEvent e) {}
+
+} */
+
+/*	public void addContentBrowsePane(){
+		
+		b1 = new JButton("Search");
+		//b1.setVerticalTextPosition(AbstractButton.CENTER);
+		//b1.setHorizontalTextPosition(AbstractButton.LEADING);
+		b1.setToolTipText("This is the first alternative of this menu.");
+		//b1.addActionListener(this);
+		
+		b2 = new JButton("Search by ID");
+		b2.setToolTipText("BEHOLD! This is the second alternative of this menu.");
+		
+		b3 = new JButton("Search by family name");
+		b3.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent event) {
+				listOfNames();
+				resultPane.setVisible(true);
+				//mainMenuPane.setVisible(false);
+				browseMenuPane.setVisible(false);
+				//frame.pack();
+				System.out.println("Edit");
+			} });
+			
+		b4 = new JButton("List by ID");
+		
+		b5 = new JButton("List by family name");
+		
+		b6 = new JButton("Info about a team");
+		
+		browseMenuPane.add(b1);
+		browseMenuPane.add(b2);
+		browseMenuPane.add(b3);
+		browseMenuPane.add(b4);
+		browseMenuPane.add(b5);
+		browseMenuPane.add(b6);
+		//frame.setContentPane(browseMenuPane);
+		//frame.getContentPane().remove(mainMenuPane);
+		//frame.getContentPane().add(browseMenuPane, BorderLayout.CENTER);
+		//browseMenuPane.setVisible(true);
+	}
+	
+	public void addContentEditPane(){
+	
+		b7 = new JButton("Create new member");
+		b8 = new JButton("Update existing member");
+		b9 = new JButton("Activate/deactivate member");
+		b10 = new JButton("Update mail");
+		b11 = new JButton("Update function");
+		b12 = new JButton("Delete member");
+		editMenuPane.add(b7);
+		editMenuPane.add(b8);
+		editMenuPane.add(b9);
+		editMenuPane.add(b10);
+		editMenuPane.add(b11);
+		editMenuPane.add(b12);
+		//editMenuPane.setVisible(true);
+		//frame.setContentPane(editMenuPane);
+		//frame.getContentPane().add(editMenuPane, BorderLayout.CENTER);
+		
+	}
+	*/
